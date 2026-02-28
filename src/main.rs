c49c27c4619713817625d704bf352c9c4e6cfc05
dhanushl0l <dhanushdjtherock@gmail.com>
2026-02-28 10:50:54 -0600

    feat: init window

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use winit::event_loop::EventLoop;
+
+use crate::app::App;
+
+mod app;
+mod state;
+
+fn main() -> anyhow::Result<()> {
+    run()
+}
+
+pub fn run() -> anyhow::Result<()> {
+    env_logger::init();
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = App::new();
+
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
+}
